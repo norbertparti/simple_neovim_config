77fc4679e35017c0dd17139f21c732b1702b2642
Norbert Parti <norbert.parti.work@gmail.com>
2024-06-07 20:28:37 +0200

    Add neotest setuo

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,15 +1,18 @@
 return {
   "nvim-neotest/neotest",
-  optional = true,
+  optional = false,
   dependencies = {
     "nvim-neotest/neotest-python",
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
   },
   opts = {
     adapters = {
       ["neotest-python"] = {
         -- Here you can specify the settings for the adapter, i.e.
-        -- runner = "pytest",
-        -- python = ".venv/bin/python",
+        runner = "pytest",
+        python = ".venv/bin/python",
       },
     },
   },
